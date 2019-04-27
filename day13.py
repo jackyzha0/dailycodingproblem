5f9c8347b621a35984642415e86c688900f859ea
unknown <jacky.zhao2019@stgeorges.bc.ca>
2019-04-27 10:25:38 -0700

    Pseudo-code for Day 13

---
+++ day13.py
@@ -0,0 +1,24 @@
+'''
+Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+
+For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+'''
+
+def longestSub(s, k):
+    #pointer 1
+    #pointer 2
+    #longest
+
+    #if pointer1 == pointer2
+    #pointer2++ until pointer1 != pointer2
+
+    #add all characters in between pointer1 and pointer2 to set
+    #pointer 2++
+    #if pointer2 in set
+    #pointer1++
+
+    #if valid and longest, update longest
+    #return length of longest
+
+inp = "abcda"
+k = 2
