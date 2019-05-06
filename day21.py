763345a4bf714bf59c30c2f7c6173a23d00671e3
unknown <jacky.zhao2019@stgeorges.bc.ca>
2019-05-06 00:34:20 -0700

    Day 21

---
+++ day21.py
@@ -0,0 +1,17 @@
+'''
+Given an array of time intervals (start, end) for classroom lectures (possibly overlapping), find the minimum number of rooms required.
+
+For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
+'''
+
+arr = [(30, 75), (0, 50), (60, 150)]
+
+def d21(arr):
+    times = [0] * (max(max(arr)))
+
+    for pair in arr:
+        times[pair[0]:pair[1]] = [x + 1 for x in times[pair[0]:pair[1]]]
+
+    return max(times)
+
+print(d21(arr))
