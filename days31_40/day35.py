23ab086ade98d21283c1c891c30a379fb137ae6f
unknown <jacky.zhao2019@stgeorges.bc.ca>
2019-05-21 19:48:59 -0700

    Day 35

--- days31_40/day35.py
+++ days31_40/day35.py
@@ -17,9 +17,20 @@ def day35_meme(arr):
 
 def swap(l, p1, p2):
     l[p1], l[p2] = l[p2],l[p1]
-    return l
 
+def day35(arr):
+    ri_r = 0
+    for ri_n in range(len(arr)):
+        if arr[ri_n] == 'R':
+            swap(arr, ri_n, ri_r)
+            ri_r += 1
+    gi_r = 0
+    for gi_n in range(len(arr)):
+        if arr[gi_n] == 'G':
+            swap(arr, gi_n, gi_r + ri_r)
+            gi_r += 1
 
+    return arr
 
-test = ['R']
+test = ['G', 'B', 'R', 'R', 'B', 'R', 'G']
 print(day35(test))
