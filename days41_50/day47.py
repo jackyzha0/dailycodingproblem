f90aa5ed453662a3ba27b74fad6233f02cdbafbd
unknown <jacky.zhao2019@stgeorges.bc.ca>
2019-05-31 10:46:37 -0700

    Day 27

---
+++ days41_50/day47.py
@@ -0,0 +1,17 @@
+'''
+Given a array of numbers representing the stock prices of a company in chronological order, write a function that calculates the maximum profit you could have made from buying and selling that stock once. You must buy before you can sell it.
+
+For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could buy the stock at 5 dollars and sell it at 10 dollars.
+'''
+
+def day47(arr):
+    mn = 1e99
+    mx = 0
+    for i in range(len(arr)):
+        if arr[i] < mn:
+            mn = arr[i]
+        mx = max(arr[i] - mn, mx)
+    return mx
+
+arr = [9, 11, 8, 5, 7, 10]
+print(day47(arr))
