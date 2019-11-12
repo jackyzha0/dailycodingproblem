b1ace247356052c4ac3cd310a07d6fe9d22649d3
jackyzha0 <j.zhao2k19@gmail.com>
2019-11-11 16:15:49 -0800

    added lc

---
+++ leetcode/kclosest.py
@@ -0,0 +1,10 @@
+import math
+
+class Solution(object):
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        return sorted(points, key=lambda coord: math.sqrt(coord[0]**2 + coord[1]**2))[0:K]
